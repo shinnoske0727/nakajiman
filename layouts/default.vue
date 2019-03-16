a0e098383ad1c86c488ad93b7b8a36afc855ca19
irie-shinnosuke <irie-shinnosuke@kayac.com>
2019-03-16 18:49:20 +0900

    defaultのlayoutでうまくやりくりする

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,28 @@
 <template lang="pug">
   div
     nuxt
-    menus
+    template(v-if="page !== 'works-id'")
+      menus
     slider
 </template>
 
 <script>
 import Menus from '@/components/menu/Menus'
 import Slider from '@/components/Slider'
+
 export default {
-  components: {
-    Menus,
-    Slider
-  }
+    components: {
+        Menus,
+        Slider
+    },
+    computed: {
+        page() {
+            return this.$route.name
+        },
+        id() {
+            return this.$route.params ? this.$route.params.id : null
+        }
+    }
 }
 </script>
 
