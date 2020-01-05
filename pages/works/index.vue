bf7bbc03567d31cd9807c099f10082696987ddf1
iemong <kiyoshidainagon@gmail.com>
2020-01-05 14:35:21 +0900

    いらんの消す

--- pages/works/index.vue
+++ pages/works/index.vue
@@ -144,11 +144,4 @@ export default {
   margin-bottom: 56px
   text-align: center
   font-size 12px
-
-.list-enter-active, .list-leave-active
-  transition: all 1s;
-
-.list-enter, .list-leave-to
-  opacity: 0;
-  transform: translateY(30px);
 </style>
