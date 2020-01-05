feb067725546e1cb51439f07a5f1df4b2bcf9194
iemong <kiyoshidainagon@gmail.com>
2020-01-05 16:48:07 +0900

    works詳細のcontentfulから取ってきたデータを組み込む

--- components/work/WorkContent.vue
+++ components/work/WorkContent.vue
@@ -1,19 +1,19 @@
 <template lang="pug">
   - path = "~/assets/img/works/";
-  .work-content(ref="content" v-if="workData.works" :data-is-picture="workData.layout === 'picture'")
+  .work-content(ref="content" v-if="workData" :data-is-picture="!workData.postType")
     .content
-      template(v-if="workData.works.kv")
-        img.kv(:src="kvSrc" :data-is-picture="workData.layout === 'picture'")
-      template(v-if="workData.layout === 'article'")
-        template(v-if="workData.name")
-          .title {{ workData.name }}
-        p.explain(v-if="workData.explain" v-html="nl2br(workData.explain)")
+      template(v-if="workData.postKv.fields.file.url")
+        img.kv(:src="kvSrc" :data-is-picture="!workData.postType")
+      template(v-if="workData.postType")
+        template(v-if="workData.postTitle")
+          .title {{ workData.postTitle }}
+        p.explain(v-if="workData.postDescription" v-html="nl2br(workData.postDescription)")
 
         .picture-wrapper
           template(v-for="pic in picSrcArray")
             img.picture(:src="pic")
-        template(v-if="workData.link")
-          a.button--site(:href="workData.link", target="_blank")
+        template(v-if="workData.postLink")
+          a.button--site(:href="workData.postLink", target="_blank")
             img(src=`${path}visit-site.svg` alt="visit site")
         button.to-top(@click="moveToTop")
           img(src=`${path}icn_arrow_top_pc.svg`)
@@ -37,17 +37,12 @@ export default {
     },
     computed: {
         kvSrc() {
-            if (!this.workData.works.kv) return
-            if (this.workData.works.detail) {
-                return require(`@/assets/data/${this.workData.works.detail}`)
-            }
-            return require(`@/assets/data/${this.workData.works.kv}`)
+            if (this.workData.postKv.fields.file.url)
+                return this.workData.postKv.fields.file.url
         },
         picSrcArray() {
-            if (!this.workData.works.pic.length) return
-            return this.workData.works.pic.map(pic =>
-                require(`@/assets/data/${pic}`)
-            )
+            if (!this.workData.postPhotos.length) return
+            return this.workData.postPhotos.map(photo => photo.fields.file.url)
         }
     },
     methods: {
@@ -58,131 +53,130 @@ export default {
             })
         },
         nl2br: function(str) {
-          str = str.replace(/\r\n/g, "<br />");
-          str = str.replace(/(\n|\r)/g, "<br />");
-          return str;
+            str = str.replace(/\r\n/g, '<br />')
+            str = str.replace(/(\n|\r)/g, '<br />')
+            return str
         }
     }
 }
 </script>
 
 <style scoped lang="stylus">
-  .work-content
-    margin: 0 auto
-    +pc-layout()
-      width: $max-width
-      padding-top: 92px
-      padding-bottom: 73px
-    +sp-layout()
-      width: $max-width-sp
-      padding-top: 64px
-      padding-bottom: 32px
-    &[data-is-picture="true"]
-      padding-bottom: 0
-  .kv
-    +pc-layout()
-      size $max-width 539px
-      margin-bottom: 96px
-    +sp-layout()
-      size $max-width-sp 193px
-      margin-bottom: 40px
+.work-content
+  margin: 0 auto
+  +pc-layout()
+    width: $max-width
+    padding-top: 92px
+    padding-bottom: 73px
+  +sp-layout()
+    width: $max-width-sp
+    padding-top: 64px
+    padding-bottom: 32px
+  &[data-is-picture="true"]
+    padding-bottom: 0
+.kv
+  +pc-layout()
+    size $max-width 539px
+    margin-bottom: 96px
+  +sp-layout()
+    size $max-width-sp 193px
+    margin-bottom: 40px
 
-    &[data-is-picture="true"]
-      display: block
-      size auto
-      max-size $max-width 85vh
-      margin: 0 auto
-      +sp-layout()
-        max-size 100%
-
-  .title
-    margin-bottom: 48px
-    font-family HiraginoSans-W6
-    font-size 20px
-    text-align: center
-    +sp-layout()
-      margin-bottom: 40px
-      font-size: 14px
-
-  .explain,
-  .role
-    width 39em
-    margin: 0 auto
-    font-size 14px
-    line-height 2
-    letter-spacing normal
-    color $text-black
-    +sp-layout()
-      font-size: 12px
-      max-width: $max-width-sp
-
-  .explain
-    word-wrap: break-word;
-    line-height: 2
-    +pc-layout()
-      margin-bottom: 96px;
-    +sp-layout()
-      margin-bottom: 48px;
-      font-size: 12px
-      line-height: 1.83
-      max-width: $max-width-sp
-
-  .picture
+  &[data-is-picture="true"]
     display: block
-    width: 100%
-
-    & + .picture
-      margin-top: 24px
-      +sp-layout()
-        margin-top: 16px
-
-  $button
-    position: relative
-    display: block
-    size 200px 40px
-    line-height: 40px
-    text-align: center
-    margin: 112px auto
-    background-color: $bg-black;
+    size auto
+    max-size $max-width 85vh
+    margin: 0 auto
     +sp-layout()
-      size 152px 36px
-      line-height: 36px
-      margin 56px auto 64px
+      max-size 100%
+
+.title
+  margin-bottom: 48px
+  font-family HiraginoSans-W6
+  font-size 20px
+  text-align: center
+  +sp-layout()
+    margin-bottom: 40px
+    font-size: 14px
+
+.explain,
+.role
+  width 39em
+  margin: 0 auto
+  font-size 14px
+  line-height 2
+  letter-spacing normal
+  color $text-black
+  +sp-layout()
+    font-size: 12px
+    max-width: $max-width-sp
+
+.explain
+  word-wrap: break-word;
+  line-height: 2
+  +pc-layout()
+    margin-bottom: 96px;
+  +sp-layout()
+    margin-bottom: 48px;
+    font-size: 12px
+    line-height: 1.83
+    max-width: $max-width-sp
+
+.picture
+  display: block
+  width: 100%
+
+  & + .picture
+    margin-top: 24px
+    +sp-layout()
+      margin-top: 16px
+
+$button
+  position: relative
+  display: block
+  size 200px 40px
+  line-height: 40px
+  text-align: center
+  margin: 112px auto
+  background-color: $bg-black;
+  +sp-layout()
+    size 152px 36px
+    line-height: 36px
+    margin 56px auto 64px
+    & > img
+      height: 10px;
+
+.button--site
+  @extend $button
+  +pc-layout()
+    &::before
+      absolute top 2px left 2px
+      content: ""
+      size: 196px 36px
+      background-color: $bg-white;
+      transform: scaleX(0)
+      transition: transform 0.35s cubic-bezier(0.22, 0.61, 0.36, 1)
+      transform-origin: left center
+    &:hover
       & > img
-        height: 10px;
+        filter: brightness(0)
 
-  .button--site
-    @extend $button
-    +pc-layout()
       &::before
-        absolute top 2px left 2px
-        content: ""
-        size: 196px 36px
-        background-color: $bg-white;
-        transform: scaleX(0)
-        transition: transform 0.35s cubic-bezier(0.22, 0.61, 0.36, 1)
-        transform-origin: left center
-      &:hover
-        & > img
-          filter: brightness(0)
+        transform: scaleX(1)
 
-        &::before
-          transform: scaleX(1)
+.to-top
+  display: block
+  size 46px 25px
+  margin: 112px auto 0
+  +pc-layout()
+    &:hover
+      img
+        animation pageTopButton 0.25s ease-in
 
-  .to-top
-    display: block
-    size 46px 25px
-    margin: 112px auto 0
-    +pc-layout()
-      &:hover
-        img
-          animation pageTopButton 0.25s ease-in
-
-    +sp-layout()
-      size 23px 13px
-      margin-top: 64px
-      background-size: contain
-      & > img
-        size 100%
-
+  +sp-layout()
+    size 23px 13px
+    margin-top: 64px
+    background-size: contain
+    & > img
+      size 100%
 </style>
