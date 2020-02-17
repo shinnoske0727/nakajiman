d6067d00d8550508254a8f100b090f4d52c1d49e
iemong <kiyoshidainagon@gmail.com>
2020-02-17 19:16:08 +0900

    cloud function upload

---
+++ functions/src/index.ts
@@ -0,0 +1,37 @@
+import * as functions from 'firebase-functions';
+const admin = require('firebase-admin');
+admin.initializeApp();
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(functions.config().api.sendgrid);
+
+export const sendMail = functions.region('asia-northeast1').https.onRequest(async (req, res) => {
+    let { message, mail, name } = req.body
+
+    const msg = {
+        to: 'nicosanco4567@gmail.com',
+        from: 'client@kenta-nakajima.com',
+        subject: '[CONTACT]クライアントからのお問い合わせ',
+        text: `
+NAME:
+${name}
+EMAIL ADDRESS:
+${mail}
+MESSAGE:
+${message}
+        `
+    };
+    await sgMail.send(msg);
+
+    res.set('Access-Control-Allow-Origin', 'http://kenta-nakajima.com/');
+    res.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    // dataフィールドに渡したい値は入れる
+    res.json({ data: 'ok' });
+});
+
+
+
+
