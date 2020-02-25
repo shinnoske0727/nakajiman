cc7aae9a1c2fca1ae087160ba0021bcafb82711f
iemong <kiyoshidainagon@gmail.com>
2020-02-25 20:02:21 +0900

    error画面のレイアウト修正

--- layouts/error.vue
+++ layouts/error.vue
@@ -5,7 +5,7 @@
       img(src=`${path}pf-404.png` alt="")
     p.text
       img(src=`${path}page-not-found.svg` alt="PAGE NOT FOUND")
-    a.button(href="/")
+    a.top-button(href="/")
       img(src=`${path}go-back.svg` alt="GO BACK")
 </template>
 
@@ -26,16 +26,19 @@ export default {
     width 100%
   +sp-layout()
     width 295px
-    margin-bottom 34px
+    margin-bottom 30px
 
 .text
   margin-bottom 54px
   line-height 1
   text-align center
   +sp-layout()
-    margin-bottom 60px
+    height 12px
+    margin-bottom 62px
+    & img
+        height 100%
 
-.button
+.top-button
   display flex
   justify-content center
   align-items center
@@ -43,4 +46,9 @@ export default {
   height 40px
   margin 0 auto
   background-color #000
+  +sp-layout()
+    width 152px
+    height 36px
+    & img
+      height 10px
 </style>
